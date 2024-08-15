e18f5706e1abf9cc2137e272ef289774a00a9db9
Juan Valero <juanvalero@cosmo.edu.co>
2024-08-14 23:02:54 -0500

    Refactor file paths for router and components, Dashboardlyout-Diseño

--- react-gpt/src/App.tsx
+++ react-gpt/src/App.tsx
@@ -1,7 +1,7 @@
 // import React from 'react'
 
 import { RouterProvider } from "react-router-dom"
-import { router } from "./router/router"
+import { router } from "./presentation/router/router"
 
 const App = () => {
     return (
---
+++ react-gpt/src/presentation/pages/index.ts
@@ -0,0 +1,10 @@
+
+export { AssistantPage } from "../pages/assistant/AssistantPage";
+export { AudioToTextPage } from "../pages/audio-to-text/AudioToTextPage"; 
+export { ImageGenerationPage } from "../pages/image-generation/ImageGenerationPage";
+export { ImageTunningPage } from "../pages/image-generation/ImageTunningPage";
+export { OrthographyPage } from "../pages/orthography/OrthographyPage";
+export { ProsConsPage } from "../pages/pros-cons/ProsConsPage";
+export { ProsConsStreamPage } from "../pages/pros-cons-stream/ProsConsStreamPage"
+export { TextToAudioPage } from "../pages/text-to-audio/TextToAudioPage";
+export { TranslatePage } from "../pages/translate/TranslatePage";
