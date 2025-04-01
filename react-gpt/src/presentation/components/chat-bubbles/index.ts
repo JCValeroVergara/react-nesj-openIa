c6c520fcaa574cba68020a7c445c2f7bc46f2b1e
Juank_Valero <jvalero2009@gmail.com>
2025-03-31 21:34:28 -0500

    feat: Add image generation and variation use cases, and update chat components to support image messages

--- react-gpt/src/presentation/components/chat-bubbles/index.ts
+++ react-gpt/src/presentation/components/chat-bubbles/index.ts
@@ -2,3 +2,5 @@ export * from './GptMessage';
 export * from './MyMessage';
 export * from './GptOrthographyMessage';
 export * from './GptMessageAudio';
+export * from './GptMessageImage';
+export * from './GptMessageSelectableImage';
